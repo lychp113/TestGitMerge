c7dc37c97bf8c4743e1d554a9c1afab4874ff3e6
李春平 <lichunping@travelsky.com>
2019-06-10 13:08:30 +0800

    "dev分支第一次添加内容"

--- src/com/travelsky/git/testGitMerge.java
+++ src/com/travelsky/git/testGitMerge.java
@@ -1,7 +1,11 @@
 package com.travelsky.git;
 
 public class testGitMerge {
+	public static void testDev() {
+		System.out.println("dev分支测试");
+	}
 	public static void main(String[] args) {
 		System.out.println("helloworld");
+		testDev();
 	}
 }
