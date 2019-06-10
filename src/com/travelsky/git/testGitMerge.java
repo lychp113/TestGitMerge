1126295f8e733eba705c310d16c83a9002399610
李春平 <lichunping@travelsky.com>
2019-06-10 14:18:02 +0800

    "dev第二次提交"

--- src/com/travelsky/git/testGitMerge.java
+++ src/com/travelsky/git/testGitMerge.java
@@ -8,6 +8,9 @@ public class testGitMerge {
 	public static void testDev() {
 		System.out.println("dev分支测试");
 	}
+	public static void testDev2() {
+		System.out.println("dev分支测试2");
+	}
 	public static void main(String[] args) {
 		System.out.println("helloworld");
 		testDev();
