c017817a60d45aaec539575f8c373dbf7d2951c7
李春平 <lichunping@travelsky.com>
2019-06-10 11:29:48 +0800

    "主分支添加的内容"

--- src/com/travelsky/git/testGitMerge.java
+++ src/com/travelsky/git/testGitMerge.java
@@ -1,7 +1,11 @@
 package com.travelsky.git;
 
 public class testGitMerge {
+	public static void testMaster() {
+		System.out.println("主分支添加的内容");
+	}
 	public static void main(String[] args) {
 		System.out.println("helloworld");
+		testMaster();
 	}
 }
