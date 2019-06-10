5e68ea0984c3e225bebadb0b18512c00cb17d553
李春平 <lichunping@travelsky.com>
2019-06-10 13:05:35 +0800

    "主分支第二次添加内容"

--- src/com/travelsky/git/testGitMerge.java
+++ src/com/travelsky/git/testGitMerge.java
@@ -3,6 +3,7 @@ package com.travelsky.git;
 public class testGitMerge {
 	public static void testMaster() {
 		System.out.println("主分支添加的内容");
+		System.out.println("主分支添加的内容2");
 	}
 	public static void main(String[] args) {
 		System.out.println("helloworld");
